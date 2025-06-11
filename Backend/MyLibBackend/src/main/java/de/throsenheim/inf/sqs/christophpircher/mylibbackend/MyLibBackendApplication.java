d10084c76549471d03d0d63dda3adc74c12de9af
Christoph Pircher <christoph.pircher@stud.th-rosenheim.de>
2025-06-11 22:49:44 +0200

    Backend project setup

---
+++ Backend/MyLibBackend/src/main/java/de/throsenheim/inf/sqs/christophpircher/mylibbackend/MyLibBackendApplication.java
@@ -0,0 +1,13 @@
+package de.throsenheim.inf.sqs.christophpircher.mylibbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLibBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyLibBackendApplication.class, args);
+    }
+
+}
---
+++ Backend/MyLibBackend/src/test/java/de/throsenheim/inf/sqs/christophpircher/mylibbackend/MyLibBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.throsenheim.inf.sqs.christophpircher.mylibbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLibBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
