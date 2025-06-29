e62510455facdb518849445912e4312edf3855b3
Christoph Pircher <christoph.pircher@stud.th-rosenheim.de>
2025-06-30 01:31:58 +0200

    Login E2E tests done

--- Playwright/code/tests/test.spec.ts
+++ Playwright/code/tests/test.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import {test, expect, Page} from '@playwright/test';
 import {text} from "node:stream/consumers";
 
 test('home page loads', async ({ page }) => {
@@ -26,9 +26,39 @@ test.describe.serial("Sign Up", () =>{
     await page.click('text=Account');
     await expect(page.locator(`text=/Logged in as:\\s*${randomUser}/`)).toBeVisible();
   });
-})
+});
 
-test.describe.serial("login", () =>{
+test.describe.serial("Login Logout", () =>{
+  const randomUser = `user${Date.now()}`;
+  test("Create user" , async ({page}) =>{
+    await signUpNewUser(randomUser, page);
+  });
 
-})
+  test("Log in and out", async ({page}) =>{
+    await page.goto("/");
+    await page.click('text=Account');
+    await page.click('text=Login');
+    await page.fill('#usernameInput', randomUser);
+    await page.fill('#passwordInput', 'testpassword1');
+    await page.click('button:has-text("Login")');
+    await page.waitForURL('**/');
+    await expect(page.locator('text=Saved books')).toBeVisible();
+    await page.click('text=Account');
+    await expect(page.locator(`text=/Logged in as:\\s*${randomUser}/`)).toBeVisible();
+    await page.click('button:has-text("Logout")');
+    await page.click('text=Account');
+    await expect(page.locator(`text="Login"`)).toBeVisible();
+    await expect(page.locator(`text="Sign Up"`)).toBeVisible();
+  });
+});
+
+const signUpNewUser = ( async (username:string, page:Page)=>{
+  await page.goto("/");
+  await page.click('text=Account');
+  await page.click('text=Sign Up');
+  await page.fill('#usernameInput', username);
+  await page.fill('#passwordInput', 'testpassword1');
+  await page.click('button:has-text("Sign Up")');
+  await page.waitForURL('**/');
+});
 
