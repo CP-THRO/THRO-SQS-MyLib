01c7b6543c72a735102236bc839b99fdcc46a45a
Christoph Pircher <christoph.pircher@stud.th-rosenheim.de>
2025-06-30 00:19:14 +0200

    getting pipeline compose to work

--- Playwright/code/playwright.config.ts
+++ Playwright/code/playwright.config.ts
@@ -22,11 +22,12 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [ ['html', { open: 'never' }] ],
+  //reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:5174',
+    baseURL: process.env.BASE_URL ?? 'http://localhost:5174',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
--- Playwright/code/tests/test.spec.ts
+++ Playwright/code/tests/test.spec.ts
@@ -9,22 +9,23 @@ test('home page loads', async ({ page }) => {
 });
 
 test.describe.serial("Sign Up", () =>{
+  const randomUser = `user${Date.now()}`; // because I need three different user for three browsers...
   test("Sign Up", async ({ page }) =>{
+    page.on('console', msg => {
+      console.log('BROWSER LOG:', msg.text());
+    });
+
     await page.goto("/");
     await page.click('text=Account');
     await page.click('text=Sign Up');
-    await page.fill('#usernameInput', 'testuser1');
+    await page.fill('#usernameInput', randomUser);
     await page.fill('#passwordInput', 'testpassword1');
     await page.click('button:has-text("Sign Up")');
     await page.waitForURL('**/');
     await expect(page.locator('text=Saved books')).toBeVisible();
     await page.click('text=Account');
-    await expect(page.locator('text=/Logged in as:\\s*testuser1/')).toBeVisible();
+    await expect(page.locator(`text=/Logged in as:\\s*${randomUser}/`)).toBeVisible();
   });
-
-
-
-
 })
 
 test.describe.serial("login", () =>{
