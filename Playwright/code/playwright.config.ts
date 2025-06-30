1f37c63c996cc7216eedbc74d7e1c5e97ad907e1
Christoph Pircher <christoph.pircher@stud.th-rosenheim.de>
2025-06-30 04:37:25 +0200

    trying to fix e2e tests

--- Playwright/code/playwright.config.ts
+++ Playwright/code/playwright.config.ts
@@ -21,6 +21,7 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
+  //workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [ ['html', { open: 'never' }] ],
   //reporter: 'html',
@@ -31,6 +32,12 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+  },
+
+  timeout: 60 * 1000,
+
+  expect: {
+    timeout: 60_000 // 60 seconds for all expect assertions
   },
 
   /* Configure projects for major browsers */
