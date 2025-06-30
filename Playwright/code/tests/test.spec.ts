3978c52eb8780e0003bb6f3f5b660c3eb38f4368
Christoph Pircher <christoph.pircher@stud.th-rosenheim.de>
2025-06-30 02:21:17 +0200

    Added Search tests

--- Playwright/code/tests/test.spec.ts
+++ Playwright/code/tests/test.spec.ts
@@ -8,7 +8,7 @@ test('home page loads', async ({ page }) => {
   await expect(page).toHaveTitle(/MyLib/);
 });
 
-test.describe.serial("Sign Up", () =>{
+test.describe("Sign Up", () =>{
   const randomUser = `user${Date.now()}`; // because I need three different user for three browsers...
   test("Sign Up", async ({ page }) =>{
     page.on('console', msg => {
@@ -29,7 +29,7 @@ test.describe.serial("Sign Up", () =>{
 });
 
 test.describe.serial("Login Logout", () =>{
-  const randomUser = `user${Date.now()}`;
+  const randomUser = `user${Date.now()}`; // do this separately so I don't have to log out before logging in
   test("Create user" , async ({page}) =>{
     await signUpNewUser(randomUser, page);
   });
@@ -52,6 +52,38 @@ test.describe.serial("Login Logout", () =>{
   });
 });
 
+test.describe("Search", () =>{
+
+});
+
+test("Search and go to details", async ({page}) =>{
+  await page.goto("/search");
+  await page.fill('#inputKeywords', 'Mass Effect');
+  await page.click('button:has-text("Search")');
+  await expect(page.locator('text=Mass Effect Ascension')).toBeVisible();
+  await expect(page.locator('table tbody tr')).toHaveCount(10);
+  await page.locator('tr', { hasText: 'Mass Effect Ascension' }).locator('text=Details').click();
+  await page.waitForURL("**/book/OL23106658M")
+  await expect(page.locator('text="Mass effect - Ascension"')).toBeVisible();
+});
+
+test("Search and add to wishlist", async ({page}) =>{
+  const randomUser = `user${Date.now()}`;
+  await signUpNewUser(randomUser, page);
+  await expect(page.locator('text="Saved books"')).toBeVisible()
+  await page.goto("/search");
+  await page.fill('#inputKeywords', 'Mass Effect');
+  await page.click('button:has-text("Search")');
+  await expect(page.locator('text=Mass Effect Ascension')).toBeVisible();
+  await expect(page.locator('tr', { hasText: 'Mass Effect Ascension' }).locator('text=Details')).toBeVisible();
+  await expect(page.locator('tr', { hasText: 'Mass Effect Ascension' }).locator('text=Add to library')).toBeVisible();
+  await expect(page.locator('tr', { hasText: 'Mass Effect Ascension' }).locator('text=Add to wishlist')).toBeVisible();
+  await page.locator('tr', { hasText: 'Mass Effect Ascension' }).locator('text=Add to wishlist ').click();
+  await expect(page.locator('text=Loading...')).toBeHidden();
+  await expect(page.locator('tr', { hasText: 'Mass Effect Ascension' }).locator('text=Add to library')).toBeVisible();
+  await expect(page.locator('tr', { hasText: 'Mass Effect Ascension' }).locator('text=Add to wishlist')).toBeHidden();
+})
+
 const signUpNewUser = ( async (username:string, page:Page)=>{
   await page.goto("/");
   await page.click('text=Account');
@@ -59,6 +91,6 @@ const signUpNewUser = ( async (username:string, page:Page)=>{
   await page.fill('#usernameInput', username);
   await page.fill('#passwordInput', 'testpassword1');
   await page.click('button:has-text("Sign Up")');
-  await page.waitForURL('**/');
 });
+
 
