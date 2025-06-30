e3888febd883a361e1c91ef018ced82479115e4d
Christoph Pircher <christoph.pircher@stud.th-rosenheim.de>
2025-06-30 16:05:54 +0200

    Hpefully this works now

--- Zap/code/test-authenticated.sh
+++ Zap/code/test-authenticated.sh
@@ -4,4 +4,4 @@ curl -X POST http://backend:8080/api/v1/auth/add-user   -H "Origin: http://front
 TOKEN=$(curl -s -X POST http://backend:8080/api/v1/auth/authenticate -H "Origin: http://frontend:80" -H "Content-Type: application/json" -d '{"username":"testuser","password":"testpassword"}')
 
 echo "Runing authenticated scan"
-python3 zap-full-scan.py -t http://frontend:80 -z "auth.header=Authorization:\ Bearer\ $TOKEN" -r full-report-authenticated.html -J full-report-authenticated.json -d
+python3 zap-full-scan.py -t http://frontend:80 -z "auth.header=Authorization:\ Bearer\ $TOKEN" -r report/full-report-authenticated.html -J report/full-report-authenticated.json -d
